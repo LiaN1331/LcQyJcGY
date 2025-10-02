8cf9b2c79c0203a664e775197173da4a8fc4ecb1
LiaN1331 <privy_fuzzed.0y@icloud.com>
2025-10-02 13:45:32 -0400

    Configure AR增强现实

---
+++ ARService_1003_0145_lqg.cs
@@ -0,0 +1,16 @@
+// 代码生成时间: 2025-10-03 01:45:31
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+using Vuforia;
+
+namespace ARApplication.Controllers
+{
+    /// <summary>
+    /// Represents the AR service controller which handles AR related requests.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]
