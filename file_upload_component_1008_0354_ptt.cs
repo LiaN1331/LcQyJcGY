cd72a2fa0d1e4ba4ea83df62ba807bb6c68272e2
LiaN1331 <privy_fuzzed.0y@icloud.com>
2025-10-07 15:54:34 -0400

    Enhance 文件上传组件

---
+++ file_upload_component_1008_0354_ptt.cs
@@ -0,0 +1,13 @@
+// 代码生成时间: 2025-10-08 03:54:33
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileUploadComponent
+{
+    /// <summary>
+    /// Controller to handle file uploads
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]
